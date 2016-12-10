6d886a8351b8652c7d6bd5497ece2eea3a25c46f
Catherine Marston <catherine@thesnowflakeapp.com>
2016-12-09 18:20:42 -0700

    added styles

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -6,25 +6,13 @@ var path = {
 }
 
 
-//gulp style task
-
-// gulp.task('sass', function() {
-//     gulp.src(path.sassSrc)
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest(path.cssSrc));
-// });
 gulp.task('styles', function(){
   gulp.src('scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./css/'));
 });
 
-//gulp watch function
-
-// gulp.task('default', function(){
-//   gulp.watch(path.sassSrc + '/**/*.scss', ['sass']);
-// });
 
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('scss/**/*.scss',['styles']);
 });
